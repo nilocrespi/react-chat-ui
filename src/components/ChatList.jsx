8051fa5846e773bd1815372eaa99866154157d0f
unknown <nilocrespi@gmail.com>
2025-09-12 13:11:06 -0300

    Feat: ChatList component structure

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,6 +1,49 @@
 const ChatList = () => {
   return (
-    <h2>ChatList funcionando</h2>
+    <ul className="contact-list">
+      <li className="contact">
+        <img src="../assets/avatar.jpg" alt="imagen de perfil"/>
+        <div>
+          <h4>Vincent Porter</h4>
+          <p className="status offline">left 7 mins ago</p>
+        </div>
+      </li>
+      <li className="contact active">
+        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <div>
+          <h4>Aiden Chavez</h4>
+          <p className="status online">online</p>
+        </div>
+      </li>
+      <li className="contact">
+        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <div>
+          <h4>Mike Thomas</h4>
+          <p className="status online">online</p>
+        </div>
+      </li>
+      <li className="contact">
+        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <div>
+          <h4>Christian Kelly</h4>
+          <p className="status offline">left 10 hours ago</p>
+        </div>
+      </li>
+      <li className="contact">
+        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <div>
+          <h4>Monica Ward</h4>
+          <p className="status online">online</p>
+        </div>
+      </li>
+      <li className="contact">
+        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <div>
+          <h4>Dean Henry</h4>
+          <p className="status offline">offline since Oct 28</p>
+        </div>
+      </li>
+    </ul>
   )
 }
 
