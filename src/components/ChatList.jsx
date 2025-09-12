0f0d40df1b15159713b8a8d6eb333855015bc59a
unknown <nilocrespi@gmail.com>
2025-09-12 14:19:06 -0300

    Fix: ChatList component image placeholder src

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -2,42 +2,42 @@ const ChatList = () => {
   return (
     <ul className="contact-list">
       <li className="contact">
-        <img src="../assets/avatar.jpg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Vincent Porter</h4>
           <p className="status offline">left 7 mins ago</p>
         </div>
       </li>
       <li className="contact active">
-        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Aiden Chavez</h4>
           <p className="status online">online</p>
         </div>
       </li>
       <li className="contact">
-        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Mike Thomas</h4>
           <p className="status online">online</p>
         </div>
       </li>
       <li className="contact">
-        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Christian Kelly</h4>
           <p className="status offline">left 10 hours ago</p>
         </div>
       </li>
       <li className="contact">
-        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Monica Ward</h4>
           <p className="status online">online</p>
         </div>
       </li>
       <li className="contact">
-        <img src="../assets/avatar.jpeg" alt="imagen de perfil"/>
+        <img src="../../public/avatar.jpeg" alt="imagen de perfil"/>
         <div>
           <h4>Dean Henry</h4>
           <p className="status offline">offline since Oct 28</p>
